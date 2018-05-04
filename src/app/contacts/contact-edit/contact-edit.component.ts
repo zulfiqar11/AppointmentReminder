bc898e2b0310a12e02b83fcb1a520120167bdf46
zulfiqar11 <35777358+zulfiqar11@users.noreply.github.com>
2018-05-04 16:27:30 -0700

    add firebase to the application

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -4,6 +4,9 @@ import { ContactsService } from '../../services/contacts.service';
 import { ActivatedRoute } from '@angular/router';
 import { Contact } from '../../Models/contact';
 
+import { AngularFireDatabase } from 'angularfire2/database'; 
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
   selector: 'app-contact-edit',
@@ -14,26 +17,31 @@ export class ContactEditComponent implements OnInit {
 
   pageTitle = 'Contact Add';
   contact: Contact;
-  debugMsg: string; 
-  timeZones= [
-    {id: 0, name: 'Select..'},
-    {id: 1, name: 'Pacific'},
-    {id: 2, name: 'Central'},
-    {id: 3, name: 'Mountain'},
-    {id: 4, name: 'Eastern'}
-  ];
+  debugMsg: string;
+  timeZones: Observable<any[]>;
+  // timeZones= [
+  //   {id: 0, name: 'Select..'},
+  //   {id: 1, name: 'Pacific'},
+  //   {id: 2, name: 'Central'},
+  //   {id: 3, name: 'Mountain'},
+  //   {id: 4, name: 'Eastern'}
+  // ];
+
+  // C:\projects\learn\angular\AppointmentReminder>npm install --save firebase angularfire2
 
   constructor(private contactService: ContactsService,
-                private route: ActivatedRoute ) { }
+                private route: ActivatedRoute,
+                private db: AngularFireDatabase ) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
     this.contact = this.contactService.getContact(id);
-    if (id > 0) this.pageTitle = 'Contact Edit';
+    if (id > 0) {this.pageTitle = 'Contact Edit'; }
+    this.timeZones = this.db.list('/TimeZone').valueChanges();
   }
 
   SaveContact() {
-    this.debugMsg= JSON.stringify(this.contact);
+    this.debugMsg = JSON.stringify(this.contact);
   }
 
 }
