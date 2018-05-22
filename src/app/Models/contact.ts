763b584e18e09ecc473f021df083246b4ed9c942
zulfiqar11 <35777358+zulfiqar11@users.noreply.github.com>
2018-05-21 18:05:05 -0700

    Converted contact interface to class

--- src/app/Models/contact.ts
+++ src/app/Models/contact.ts
@@ -1,9 +1,18 @@
 
-export interface Contact {
+export class Contact {
     id: number;
     FirstName: string;
     LastName: string;
     Email: string;
     Phone: string;
     TimeZone: string;
+
+    constructor ( id: number, firstName: string, lastName: string,  phone: string, email: string, timezone: string)  {
+        this.id = name;
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Email = email;
+        this.Phone = phone;
+        this.TimeZone = timezone;
+    }
 }
