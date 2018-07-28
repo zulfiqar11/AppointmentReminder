42738d96f185313e8d5cbd36554ef5ce0eabe900
zulfiqar11 <35777358+zulfiqar11@users.noreply.github.com>
2018-07-28 00:50:57 +0000

    clean up reset password page.

--- src/app/authentication/reset-password/reset-password.component.ts
+++ src/app/authentication/reset-password/reset-password.component.ts
@@ -1,5 +1,7 @@
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '../../../../node_modules/@angular/router';
+
 
 @Component({
   selector: 'app-reset-password',
@@ -8,14 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   onReset(resetFormData): void {
     this.authService.resetPassword(resetFormData.value.email).then(() => {
-        alert('Reset instruction sent to your mail');
+        alert('For reset instructions please check your email =>  ' + resetFormData.value.email + ' . Please do not forget to check spam folder.. just in case');
+        this.router.navigateByUrl('/login');
     }).catch((error) => {
         // this.errorMessage = error.message;
         // this.showError = true;
