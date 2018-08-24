e10fa19652944c26059d1f59d09574e5b2ea5dac
zulfiqar11 <35777358+zulfiqar11@users.noreply.github.com>
2018-08-24 14:48:46 +0000

    database call cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,5 +4,21 @@ import * as functions from 'firebase-functions';
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase Zulfiqar and sobia!");
+ response.send("Hello from Firebase Zulfiqar!");
+
+  // Import Admin SDK
+  var admin = require("firebase-admin");
+  admin.initializeApp(functions.config().firebase);
+
+  // Get a database reference to our posts
+  var db = admin.database();
+  var ref = db.ref("users/reminders/BEGXFKfOBlagPRdYZ106kRz4MkD3/-LDs_otK4Az2_Ee0KXym/contmobile");
+
+  // Attach an asynchronous callback to read the data at our posts reference
+  ref.on("value", function(snapshot) {
+    console.log(snapshot.val());
+    console.log("im here 123");
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
 });
